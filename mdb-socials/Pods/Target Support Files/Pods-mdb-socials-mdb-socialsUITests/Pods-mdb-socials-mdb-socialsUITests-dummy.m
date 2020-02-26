a3503411d35b87e536b2a6e96cb34b0af5b373ed
James <jameswsj@berkeley.edu>
2020-02-26 00:28:46 -0800

    adding firebase

---
+++ mdb-socials/Pods/Target Support Files/Pods-mdb-socials-mdb-socialsUITests/Pods-mdb-socials-mdb-socialsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mdb_socials_mdb_socialsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_mdb_socials_mdb_socialsUITests
+@end
---
+++ mdb-socials/Pods/Target Support Files/Pods-mdb-socialsTests/Pods-mdb-socialsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mdb_socialsTests : NSObject
+@end
+@implementation PodsDummy_Pods_mdb_socialsTests
+@end
