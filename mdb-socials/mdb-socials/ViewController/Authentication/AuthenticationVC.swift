7f23f5738fb9cc9bdad28dfcd54642b1f5066062
Alvaro Morales <Ag1mor@berkeley.edu>
2020-03-01 19:01:11 -0800

    Dealing with more merge conflict stuff

--- mdb-socials/mdb-socials/ViewController/Authentication/AuthenticationVC.swift
+++ mdb-socials/mdb-socials/ViewController/Authentication/AuthenticationVC.swift
@@ -49,52 +49,5 @@ class AuthenticationVC: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
  
-    /*
-    func handleRegister() {
-        guard let name = nameTextField.text else {
-            return
-        }
-        guard let number = phoneNumberTextField.text else {
-            return
-        }
-        guard let email = emailTextField.text else {
-            return
-        }
-        guard let password = passwordTextField.text else {
-            return
-        }
-        // create user
-        let auth = Auth.auth()
-        auth.createUser(withEmail: email, password: password) { (user, error) in
-            guard error == nil else {
-                self.displayAlert(title: "Error", message: "Create User yoasted")
-                return
-            }
-            for (userId, _) in usersDict {
-                guard let userInfoDict = usersDict[userId] else {
-                    print("friends error 2")
-                    continue
-                }
-                
-                if userInfoDict["email"]! == email {
-                    let theUsersName = userInfoDict["name"]
-                    UserDefaults.standard.set(theUsersName, forKey: "name")
-                }
-                
-            }
-        })
-        
-            
-        self.performSegue(withIdentifier: "toFeedScreen", sender: self)
-        }
-    }
-    
-    func displayAlert(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
-        alert.addAction(defaultAction)
-        self.present(alert, animated: true, completion: nil)
-    }
-
 }
 
